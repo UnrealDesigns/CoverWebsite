fcebbcd46e39b2d07e124beebb0c9ea0a704078e
Vikrant Pogula <vikrant.pogula@gmail.com>
2013-08-15 18:07:49 +0530

    Basic Dynamic Layout - 960px only

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
 /* Main JS File */
+
+angular.module('cover', []).config(function($routeProvider){
+	$routeProvider.
+		when('/', {controller:MainCtrl, templateUrl:'views/home.html'}).
+		otherwise({redirectTo:'/'});
+});
+
+function MainCtrl($scope){
+	$scope.name = "Vikrant";
+
+	$scope.navLinks = [
+						{href:"#", name:"Home"},
+						{href:"#", name:"Home"},
+						{href:"#", name:"Home"},
+						{href:"#", name:"Home"}
+	];
+
+	$scope.contentBlocks = [
+						{image:"#", imagealt:"POST1", title:"Post 1", description:"This is the first post"},
+						{image:"#", imagealt:"POST2", title:"Post 2", description:"This is the second post"},
+	];
+}
